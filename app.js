7276fcf508799f5282f98a2c9d45b32278aa26d0
Sergey Filkin <fs@alternativa-pro.ru>
2016-02-01 14:16:56 +0300

    Fixed start answer

--- app.js
+++ app.js
@@ -6,22 +6,31 @@ var bot = new TelegramBot(config.token, {polling: true});
 // Forwards message to another chat
 bot.on('message', function (msg) {
 
-  	var privateChatID = msg.chat.id;
+    if (msg.text === "/start") {
+        return;
+    }
+
+    var privateChatID = msg.chat.id;
   	var fromId = msg.from.id;
 
-  	var senderName = msg.from.first_name + " " + msg.from.last_name + " @" + msg.from.username;
-  	// redefines senderName if no last name or username were found
-  	if (msg.from.last_name === undefined) {
-  		senderName = msg.from.first_name;
-  	}
-  	else if (msg.from.username === undefined) {
-  		senderName = msg.from.first_name + msg.from.last_name;
-  	}
+    var sender = msg.from;
+  	var senderName = "Отправитель: ";
+    if (sender.first_name !== undefined) {
+        senderName = senderName + sender.first_name;
+    }
+    if (sender.last_name !== undefined) {
+        senderName = senderName + " " + sender.last_name;
+    }
+    if (sender.username !== undefined) {
+        senderName = senderName + " @" + sender.username;
+    }
+    senderName = senderName + " ("+fromId+")\n";
 
-  	var botMsg = senderName + " написал(а): " + msg.text;
-	bot.sendMessage(config.chatid, botMsg);
+    // пересылаем обращение в наш закрытый чат
+    var botMsg = senderName +"Сообщение: " + msg.text;
+    bot.sendMessage(config.chatid, botMsg);
 
-	var reply = senderName + ", " + config.answer;
-	bot.sendMessage(privateChatID, reply);
+    // отправляем подтверждение, что обращение принято
+	bot.sendMessage(privateChatID, config.answer);
 
 });
