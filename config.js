f320518207cb92913acba731ebf1fa7f0f1cd54d
Sergey Filkin <FSerg@users.noreply.github.com>
2016-05-04 14:06:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    'token':process.env.TOKEN ||,
-    'chatid':process.env.CHATID ||,
+    'token':process.env.TOKEN,
+    'chatid':process.env.CHATID,
     'time':process.env.TIME || 600000, // in miliseconds
     'answer':process.env.ANSWER || "Спасибо, ваше обращение принято.\nМы постараемся ответить в ближайшее время..."
 };
